7061327562bce81f35ba74e693102bf9e32547c4
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-04-25 16:49:25 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,3 @@
+if [[ "${NO_HEADER}" = "Enable" ]]; then
+    sed -i 's@#header{height:25rem;position:relative;background:url(/assets/cover.5733800e.png) no-repeat center center}#header:before{content:"";position:absolute;top:0;left:0;bottom:0;right:0;z-index:1;width:100%;background:rgba(66,64,64,.15)}#header:after{content:"Pixiv: 86597206";position:absolute;right:15px;bottom:0;z-index:2;color:#cdcdcd;text-shadow:1px 1px 1px #666}@@;s@#header{height:20rem;background:url(/assets/cover_mobile.27fb488e.png) no-repeat center center!important}@@' ./node_modules/nodestatus-server/build/dist/hotaru-theme/assets/index*.css
+fi
